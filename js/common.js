8f698a12826d7d3a377322104a8534b72d6b1f46
Alexander Bukreev <padmasherab@icloud.com>
2018-03-23 19:41:53 +0300

    modal

--- js/common.js
+++ js/common.js
@@ -15,3 +15,29 @@ function hideTopSubMunu(margingTop, id1, id2, visibleClass) {
         navbarTopChekcbox.checked = false;
     }
 }
+
+// modal 
+var modalId;
+function getModal(id) {
+	modalId = id;
+	
+	var MAIN_MODAL = document.getElementById('main-modal');
+	MAIN_MODAL.classList.toggle('display-none');
+
+	var visibleBlock = document.getElementById(modalId);
+	visibleBlock.classList.toggle('display-none');
+}
+
+function closeBtn() {
+	MAIN_MODAL = document.getElementById('main-modal');
+	MAIN_MODAL.classList.toggle('display-none');
+
+	var visibleBlock = document.getElementById(modalId);
+	visibleBlock.classList.toggle('display-none');
+}
+/*
+window.onclick = function(event) {
+    MAIN_MODAL = document.getElementById('main-modal');
+    MAIN_MODAL.classList.toggle('display-none');
+}
+*/
